72fb7c8bb698a3129245720866f1afe15beadd11
MaksymPetyak <petyak.mi@gmail.com>
2023-09-27 17:01:56 +0200

    Adapts the core dataset class to return an iterator

--- medplexity/datasets/dataset.py
+++ medplexity/datasets/dataset.py
@@ -14,23 +14,20 @@ class Dataset:
 
     def __init__(self, data_points: List[DataPoint], description: str = ""):
         """
-        Initialize the dataset with data and labels.
+        Initialize the dataset with data points.
 
         Parameters:
-        - data: List of data points
-        - labels: List of labels corresponding to the data points
+        - data_points: List of data points
         """
 
-        self.index = 0
         self.data_points = data_points
-
         self._description = description
 
     def __iter__(self):
         """
-        Return the iterator object (in this case, self).
+        Return the iterator object.
         """
-        return self
+        return iter(self.data_points)
 
     def __len__(self):
         return len(self.data_points)
@@ -39,15 +36,5 @@ class Dataset:
     def description(self):
         return self._description
 
-    def __next__(self) -> DataPoint:
-        """
-        Return the next data point and label.
-        """
-        if self.index >= len(self):
-            raise StopIteration
-
-        data_point = self.data_points[self.index]
-
-        self.index += 1
-
-        return data_point
+    def __getitem__(self, idx):
+        return self.data_points[idx]
