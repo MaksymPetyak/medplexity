461452c22a0e9156451922fcd7526e47abdf9532
MaksymPetyak <petyak.mi@gmail.com>
2023-09-27 17:02:04 +0200

    Add small prompt template for question answering

---
+++ medplexity/prompts/medical_assistant_prompt_template.py
@@ -0,0 +1,16 @@
+
+class MedicalAssistantPromptTemplate():
+    """Example prompt for long-form question answering, from https://arxiv.org/abs/2305.09617"""
+
+    PROMPT = '''You are a helpful medical knowledge assistant. Provide useful, complete, and scientifically-grounded answers to common
+consumer search queries about health.
+
+Question: {question}
+Complete Answer:'''
+
+
+    def format(self, question: str):
+        return self.PROMPT.format(
+            question=question,
+        )
+
