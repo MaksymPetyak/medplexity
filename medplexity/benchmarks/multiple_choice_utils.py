08afa45f5efe5a51e94d876a5de8c46a04991c8f
MaksymPetyak <petyak.mi@gmail.com>
2023-09-28 12:46:21 +0200

    Reformatting

--- medplexity/benchmarks/multiple_choice_utils.py
+++ medplexity/benchmarks/multiple_choice_utils.py
@@ -13,28 +13,35 @@ class MultipleChoicePromptExample(BaseModel):
 
 MultipleChoiceExampleQuestions = RootModel[List[MultipleChoicePromptExample]]
 
+
 class AnswerWithExplanation(BaseModel):
     answer: str
     explanation: str
 
 
 INDEX_TO_OPTION = {0: "(A)", 1: "(B)", 2: "(C)", 3: "(D)", 4: "(E)"}
-def format_options(options: List[str]) -> str:
 
+
+def format_options(options: List[str]) -> str:
     return " ".join(
         [f"{INDEX_TO_OPTION[i]} {option}" for i, option in enumerate(options)]
     )
+
 
 def format_answer_to_letter(options: List[str], answer: str) -> str:
     answer_idx = options.index(answer)
 
     return INDEX_TO_OPTION[answer_idx]
 
-def load_example_questions_from_json(file_path: Path | str) -> MultipleChoiceExampleQuestions:
+
+def load_example_questions_from_json(
+    file_path: Path | str,
+) -> MultipleChoiceExampleQuestions:
     with open(file_path, "r") as f:
         examples = MultipleChoiceExampleQuestions.model_validate_json(f.read())
 
         return examples
+
 
 def build_example_questions(file_path: Path | str) -> List[str]:
     examples = load_example_questions_from_json(file_path)
@@ -50,5 +57,3 @@ def build_example_questions(file_path: Path | str) -> List[str]:
         )
         for example in examples.root
     ]
-
-
