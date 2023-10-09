bd194981a407caf8205fa0f59fae657cde2091b5
MaksymPetyak <petyak.mi@gmail.com>
2023-10-09 17:57:36 +0200

    Removes MedMCQA loaders

--- medplexity/benchmarks/medmcqa/medmcqa_dataset_factory.py
+++ medplexity/benchmarks/medmcqa/medmcqa_dataset_factory.py
@@ -1,10 +1,9 @@
+from typing import Literal
+
+from datasets import load_dataset
 from pydantic import BaseModel
 
 from medplexity.benchmarks.dataset_factory import DatasetFactory
-from medplexity.benchmarks.medmcqa.medmcqa_loader import (
-    MedMCQALoader,
-    MedMCQADatasetSplitType,
-)
 from medplexity.datasets.dataset import DataPoint, Dataset
 
 
@@ -24,6 +23,9 @@ class MedMCQADataPoint(DataPoint):
     metadata: MedMCQAOutputMetadata
 
 
+MedMCQADatasetSplitType = Literal["train", "validation", "test"]
+
+
 class MedMCQADatasetFactory(DatasetFactory):
     """Multiple-choice questions designed to address real-world medical entrance exam questions like AIIMS & NEET PG.
     This dataset encompasses over 194k high-quality MCQs spanning 2.4k healthcare topics and 21 medical subjects. Questions are accompanied by an explanation of the correct answer.
@@ -38,13 +40,10 @@ class MedMCQADatasetFactory(DatasetFactory):
     Dataset version used: <https://huggingface.co/datasets/medmcqa>
     """
 
-    def __init__(self, loader: MedMCQALoader | None = None):
-        self.loader = loader or MedMCQALoader()
-
     def build_dataset(
         self, split_type: MedMCQADatasetSplitType
     ) -> Dataset[MedMCQADataPoint]:
-        questions = self.loader.load_questions(split_type)
+        questions = load_dataset("medmcqa", split=split_type)
 
         data_points = [
             MedMCQADataPoint(
