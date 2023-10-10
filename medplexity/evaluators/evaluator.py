ad00e2a43c1b00504f8907c6d99ae308a7b91a02
MaksymPetyak <petyak.mi@gmail.com>
2023-10-10 18:55:25 +0200

    Adds medharness class and new chain for mulitple choice evaluation

--- medplexity/evaluators/evaluator.py
+++ medplexity/evaluators/evaluator.py
@@ -1,6 +1,7 @@
 import abc
 from typing import Optional
 
+from medplexity.chains.chain import Chain
 from medplexity.datasets.dataset import Dataset
 from medplexity.evaluators.evaluation_summary import EvaluationResult
 
@@ -8,7 +9,11 @@ from medplexity.evaluators.evaluation_summary import EvaluationResult
 class Evaluator(abc.ABC):
     @abc.abstractmethod
     def evaluate(
-        self, dataset: Dataset, max_items: Optional[int] = None
+        self,
+        dataset: Dataset,
+        chain: Chain,
+        max_items: Optional[int] = None,
+        ignore_errors: bool = False,
     ) -> EvaluationResult:
         """Evaluate the LLM on the dataset and return the results."""
         raise NotImplementedError("Evaluator evaluate method is not implemented")
---
+++ medplexity/medharness.py
@@ -0,0 +1,63 @@
+from medplexity.chains.chain import Chain
+from medplexity.datasets.dataset import Dataset
+from medplexity.evaluators.evaluation_summary import EvaluationSummary
+from medplexity.evaluators.evaluator import Evaluator
+from medplexity.evaluators.sequential_evaluator import SequentialEvaluator
+
+
+class Medharness:
+    """Medharness main class for evaluation of given dataset and chain."""
+
+    def __init__(
+        self,
+        dataset: Dataset,
+        chain: Chain,
+    ):
+        self.dataset = dataset
+        self.chain = chain
+
+        self.result: EvaluationSummary | None = None
+
+    def run(
+        self, max_k: int | None = None, evaluator: Evaluator = SequentialEvaluator()
+    ) -> EvaluationSummary:
+        """Run the evaluation on the dataset with the provided chain. Results are stored in self.result.
+
+        Args:
+            max_k (int | None, optional): Limit the dataset to the first max_k items.
+                Defaults to None, which means the entire dataset will be used.
+            evaluator (Evaluator, optional): Evaluator to be used for the evaluation.
+                Defaults to SequentialEvaluator.
+
+        Returns:
+            EvaluationSummary: Results of the evaluation.
+        """
+
+        if max_k:
+            dataset = self.dataset[:max_k]
+        else:
+            dataset = self.dataset
+
+        result = evaluator.evaluate(
+            dataset=dataset,
+            chain=self.chain,
+        )
+
+        self.result = result
+
+        return result
+
+    def save_results(self, filename: str, additional_data: dict = None):
+        if self.result is None:
+            raise ValueError("No result to save.")
+
+        self.result.save(filename, additional_data=additional_data)
+
+    @property
+    def result(self) -> EvaluationSummary:
+        """Report the results of the benchmarks."""
+        return self.result
+
+    @result.setter
+    def result(self, value):
+        self._result = value
