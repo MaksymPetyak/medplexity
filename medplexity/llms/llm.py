54822ce5323181bde5f9acaa42498d402916e13d
MaksymPetyak <petyak.mi@gmail.com>
2023-11-15 16:26:38 +0100

    Make model a property required for each llm

--- medplexity/llms/llm.py
+++ medplexity/llms/llm.py
@@ -7,5 +7,7 @@ load_dotenv()
 class LLM(abc.ABC):
     """Base class for LLMs."""
 
+    model: str
+
     def __call__(self, instruction: str, suffix: str = "") -> str:
         raise NotImplementedError("LLM call method is not implemented")
